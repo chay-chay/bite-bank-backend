cfe2240e199ccaa1ca8dae8f6cf59a16d23ab5a0
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-03-15 22:21:13 -0400

    recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,45 @@
 class RecipesController < ApplicationController
 
+    # GET /recipes
     def index
-        @recipes = Recipe.all
-        render json: @recipes
+        recipes = Recipe.all
+        render json: recipes
     end
+
+     # GET /recipes/1
+     def show
+        recipe = Recipe.find(params[:id])
+        render json: recipe
+    end
+
+    # POST /recipes
+    def create
+        recipe = Recipe.create(recipe_params)   
+        render json: recipe
+    end
+
+     # PATCH/PUT /recipes/1
+    def update
+        recipe = Recipe.find(params[:id])
+        recipe.update(recipe_params)
+        render json: recipe     
+    end
+
+    # DELETE /recipes/1
+    def destroy
+        recipe = Recipe.find(params[:id])
+        recipe.destroy
+        render json: recipe
+    end
+
+
+    private
+
+      def recipe_params
+        params.require(:recipe).permit(:name, :category, :area, :image_url, :youtube_url )
+      end
+      
+
+
+
 end
