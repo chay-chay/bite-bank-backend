925624728e485fea4867d2cb5fb0012c46c79344
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-07 12:29:32 -0400

    add validates & error full_messages

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ApplicationRecord
     validates :name, presence: true, :uniqueness => {:case_sensitive => false}
+    validates :category, presence: true
+    validates :area, presence: true
+    validates :image_url, presence: true
+    validates :youtube_url, presence: true
 end
