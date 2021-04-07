1fca287342e44dd3a5bf3baf1950280f5eebc3a0
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-06 21:31:57 -0400

    add Gem pg and changed to Postgre

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://chay-chay.github.io/bite-bank-client/'
 
     resource '*',
       headers: :any,
