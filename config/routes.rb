664a6acbe4b17e0f40f26b67423a515162a91f88
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-06 22:40:06 -0400

    hosted on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-root "recipes#index"
+root "bitebank"
   resources :recipes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
