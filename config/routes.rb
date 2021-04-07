ea9f03319c1f34969148e6cf14bd46254c81907f
Chayanit McCormick <chayanit.chaisri@gmail.com>
2021-04-06 23:06:50 -0400

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # root "recipes#index"
-  resources :recipes, as: :root
+  root "recipes#index"
+  resources :recipes
 
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
